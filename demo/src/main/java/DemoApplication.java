f2d907c7e1ac718bb963c69c623dbb6e146aadb9
Guilherme Martins <guimsl2004@gmail.com>
2024-02-26 16:01:29 -0300

    tudo errado

--- demo/src/main/java/DemoApplication.java
+++ demo/src/main/java/DemoApplication.java
@@ -1,5 +1,8 @@
 package main.java.guilherme;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class DemoApplication {
 
--- demo/src/main/java/Resurces/UserResurce.java
+++ demo/src/main/java/Resurces/UserResurce.java
@@ -1,6 +1,16 @@
 package main.java.Resurces;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import main.java.Entities.Users;
+
+@RestController
+@RequestMapping(value = "/users")
 public class UserResurce {
     
+    public ResponseEntity<Users> findAll(){
+        
+    }
 }

40abf2a3f2821647a3a90a533051c2f6ff655ca1
Guilherme Martins <guimsl2004@gmail.com>
2024-03-04 16:04:36 -0300

    banco de dados teste h2

--- demo/src/main/java/com/guilherme/demo/entities/User.java
+++ demo/src/main/java/com/guilherme/demo/entities/User.java
@@ -2,7 +2,17 @@ package com.guilherme.demo.entities;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;  
     private String email;

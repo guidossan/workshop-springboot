6397e32eed0a84587897b56d3db85e37bd6d3ac2
Guilherme Martins <guimsl2004@gmail.com>
2024-03-04 16:46:26 -0300

    find by id

---
+++ demo/src/main/java/com/guilherme/demo/Services/UserServices.java
@@ -0,0 +1,26 @@
+package com.guilherme.demo.Services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.guilherme.demo.Repositorie.UserRepositorie;
+import com.guilherme.demo.entities.User;
+
+@Service
+public class UserServices {
+    
+    @Autowired
+    private UserRepositorie repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
--- demo/src/main/java/com/guilherme/demo/resurces/UserResurce.java
+++ demo/src/main/java/com/guilherme/demo/resurces/UserResurce.java
@@ -2,13 +2,16 @@ package com.guilherme.demo.resurces;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.guilherme.demo.Services.UserServices;
 import com.guilherme.demo.entities.User;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 
@@ -16,9 +19,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequestMapping(value ="/users")
 public class UserResurce {
 
+    @Autowired
+    private UserServices userService;
     @GetMapping
-    public ResponseEntity<User> findall(){
-        User u = new User(1L, "guilherme", "gui@gmail.com", "14999992031");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findall(){
+        List<User> list = userService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User obj = userService.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }

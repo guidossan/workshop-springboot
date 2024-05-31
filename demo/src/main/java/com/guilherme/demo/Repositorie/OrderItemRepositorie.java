705b6a66136b686f37d31236817c0881b2afc6f1
Guilherme Martins <guimsl2004@gmail.com>
2024-05-31 14:07:53 -0300

    orders

--- demo/src/main/java/com/guilherme/demo/Repositorie/OrderItemRepositorie.java
+++ demo/src/main/java/com/guilherme/demo/Repositorie/OrderItemRepositorie.java
@@ -3,8 +3,9 @@ package com.guilherme.demo.Repositorie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.guilherme.demo.entities.OrderItem;
+import com.guilherme.demo.entities.entitiesPk.OrderItemPk;
 
-public interface OrderItemRepositorie extends JpaRepository<OrderItem, Long>{
+public interface OrderItemRepositorie extends JpaRepository<OrderItem, OrderItemPk>{
     
     
 }

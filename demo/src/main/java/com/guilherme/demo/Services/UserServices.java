214ab64217afb16329ee373b5bd7637375a1054c
Guilherme Martins <guimsl2004@gmail.com>
2024-05-31 15:27:56 -0300

    delete user

--- demo/src/main/java/com/guilherme/demo/Services/UserServices.java
+++ demo/src/main/java/com/guilherme/demo/Services/UserServices.java
@@ -27,4 +27,7 @@ public class UserServices {
     public User insert(User obj){
         return repository.save(obj);
     }
+    public void delete(Long id){
+        repository.deleteById(id);
+    }
 }
